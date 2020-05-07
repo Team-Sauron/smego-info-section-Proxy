0b7bfe663b3e09b2298efd74600a3ca0857f60e5
carlitoswillis <carlitoswillis@berkeley.edu>
2020-05-07 12:35:42 -0700

    retrieve bundle from each publc folder

---
+++ server/index.js
@@ -0,0 +1,30 @@
+// import
+const express = require('express');
+const path = require('path');
+
+// init
+
+const app = express();
+
+// static
+app.use(express.static(`${__dirname}/../public/`));
+
+// middleware
+
+app.use(express.json());
+
+// endpoints
+
+// listen
+const port = 2999;
+
+app.listen(port, (err) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.log('uh oh');
+    throw err;
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`listening at http://localhost:${port}`);
+  }
+});
